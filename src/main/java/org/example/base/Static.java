4c269932a6780de3c7d57da825f0257c0e582189
kang <1115610574@qq.com>
2021-07-17 11:42:18 +0800

    init

---
+++ src/main/java/org/example/base/Static.java
@@ -0,0 +1,12 @@
+package org.example.base;
+
+public class Static {
+    public static String init = new String("hello");
+
+     static {
+     }
+
+     public static void something() {
+         System.out.println("out put something");
+     }
+}
